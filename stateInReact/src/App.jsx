67aaa00c2853e00eade9439d0dcd49a2f915e81e
Sushil <sushildangoriya40@gmail.com>
2025-10-04 19:30:59 +1000

    hooks example completed along with useEffect hook featuring side-effects and dependencies

--- stateInReact/src/App.jsx
+++ stateInReact/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Card from "./Card";
+import HookExample from "./hooks/hookExample";
 
 function App() {
   // Why state ?
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Card />
+      <HookExample />
     </>
   );
 }
---
+++ stateInReact/src/hooks/HookExample.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+const HookExample = () => {
+  const [count, setCount] = useState(0);
+  const [decValue, setDecValue] = useState(0);
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setDecValue((prev) => prev - 1);
+  };
+
+  useEffect(() => {
+    // alert("Hello I am using useEffect hook");
+    console.log("Hello how are you?");
+  }, [count]);
+  return (
+    <div>
+      <h1>hookExample</h1>
+      <div className="hookExample">{count}</div>
+      <button
+        style={{ border: "1px", backgroundColor: "green" }}
+        onClick={increment}
+      >
+        Increment from Hook
+      </button>
+      <div className="hookExample">{decValue}</div>
+      <button
+        style={{ border: "1px", backgroundColor: "yellow" }}
+        onClick={decrement}
+      >
+        Decrement from Hook
+      </button>
+    </div>
+  );
+};
+
+export default HookExample;
