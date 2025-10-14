a60c10e11afba070c412b551f3772f053709539e
Sushil <sushildangoriya40@gmail.com>
2025-10-14 21:05:02 +1000

    stopPropagation and stopPreventDefault

--- eventHandling/src/App.jsx
+++ eventHandling/src/App.jsx
@@ -7,6 +7,17 @@ function App() {
   const hnadleOnClick = (name) => {
     console.log(`${name} hello how are you?`);
   };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit event triggered.");
+  };
+  const parentHandler = (e) => {
+    console.log("Parent Clicked");
+  };
+  const childHandler = (e, name) => {
+    e.stopPropagation();
+    console.log(`${name} Child Clicked`);
+  };
   return (
     <div className="wrapper flex-col justify-center items-center mt-5 gap-5">
       <div className="bg-red-500 p-5">
@@ -30,6 +41,27 @@ function App() {
           className="bg-black text-white border-2 rounded-2xl"
         >
           ClickWithPassingReference
+        </button>
+      </div>
+
+      <form className="flex gap-3" onSubmit={handleOnSubmit}>
+        <input
+          className="border-2 border-sky-400 rounded p-2"
+          type="text"
+          placeholder="Enter your name"
+        />
+        <button>Submit</button>
+      </form>
+      <div onClick={parentHandler} className="mt-5 p-5 bg-amber-500">
+        ParentClick{" "}
+        <button
+          className="bg-red-400 p-5"
+          // onClick={childHandler}
+          onClick={(e) => {
+            childHandler(e, "Sushil");
+          }}
+        >
+          ChildClick
         </button>
       </div>
     </div>
