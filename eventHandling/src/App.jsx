d3c793705565f121c66786e3ef2723008ef1d472
Sushil <sushildangoriya40@gmail.com>
2025-10-14 20:45:51 +1000

    passing reference in event handling

--- eventHandling/src/App.jsx
+++ eventHandling/src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 
 function App() {
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+  };
+  const hnadleOnClick = (name) => {
+    console.log(`${name} hello how are you?`);
+  };
   return (
     <div className="wrapper flex-col justify-center items-center mt-5 gap-5">
       <div className="bg-red-500 p-5">
@@ -12,6 +18,18 @@ function App() {
           className="bg-blue-400 rounded p-3"
         >
           Click
+        </button>
+        <input
+          className="border-sky-100"
+          onChange={handleOnChange}
+          type="text"
+          placeholder="Type here"
+        />
+        <button
+          onClick={() => hnadleOnClick("Sushil")}
+          className="bg-black text-white border-2 rounded-2xl"
+        >
+          ClickWithPassingReference
         </button>
       </div>
     </div>
