bf2dc5ec223765eaf7f0c367edbab0da27aacd05
sushil-distrosub <sushil@distrosub.com>
2025-11-01 19:32:52 +1000

    adding uncontrolled component

---
+++ handleInputField/src/App.jsx
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const inputRef = useRef(null);
+  const handleOnChange = (e) => {
+    setName(e.target.value);
+  };
+  console.log(name);
+  function submit() {
+    // console.log("hello Address!");
+    console.log(inputRef);
+    console.log(inputRef.current.value);
+  }
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter name"
+          onChange={handleOnChange}
+        />
+        <br />
+        <br />
+        <button onClick={() => setName("")}>Clear</button>
+        <h1>{name}</h1>
+      </div>
+      <div>
+        <input ref={inputRef} type="text" placeholder="Enter Address" />
+        <br />
+        <br />
+        <button onClick={submit}>Submit</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
