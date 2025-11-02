e4effa1f34a579c8b0bbfc59b6bbe493757609dc
sushil-distrosub <sushil@distrosub.com>
2025-11-02 19:06:04 +1000

    adding nested rendering using ternary operator

--- conditionalRendering/src/App.jsx
+++ conditionalRendering/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  const isLogin = true;
+  const option = "a";
+  // const isLogin = true;
   // if (isLogin) {
   //   return (
   //     <div>
@@ -14,7 +15,18 @@ function App() {
   //     <h1>Please login!</h1>
   //   </div>
   // );
-  return <> {isLogin ? <h1>Logged In</h1> : <h1>Please Log in</h1>}</>;
+  // return <> {isLogin ? <h1>Logged In</h1> : <h1>Please Log in</h1>}</>;
+  return (
+    <>
+      {option === "a" ? (
+        <h1>Component A</h1>
+      ) : option === "b" ? (
+        <h1>Component B</h1>
+      ) : (
+        <h1>Other Component</h1>
+      )}
+    </>
+  );
 }
 
 export default App;
